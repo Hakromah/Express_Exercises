dd0a70345f2b30c3c6a17bf4c985be3a01d85632
Hassan <kkromah12@gmail.com>
2023-02-18 16:04:12 +0300

    done

--- dist/server.js
+++ dist/server.js
@@ -1,16 +1,26 @@
+import * as dotenv from 'dotenv';
 import express from 'express';
 import "express-async-errors";
 import morgan from "morgan";
-import { getAll, getUser, getPost, getUpdate, getDelete } from "./controllers/planets.js";
+dotenv.config();
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 app.use(morgan("dev"));
 app.use(express.json());
-app.get("/api/planets", getAll);
-app.get("/api/planets/:id", getUser);
-app.post("/api/planets", getPost);
-app.put("/api/planets/:id", getUpdate);
-app.delete("/api/planets/:id", getDelete);
+let planets = [
+    {
+        id: 1,
+        name: "Earth",
+    },
+    {
+        id: 2,
+        name: "Mars",
+    },
+];
+app.get('/', (req, res) => {
+    console.log(req);
+    res.status(200).json(planets);
+});
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,42 @@
 import * as dotenv from 'dotenv'
-
 import express
 from 'express';
 import "express-async-errors";
 import morgan from "morgan";
-//import {getAll, getUser, getPost, getUpdate, getDelete} from "./controllers/planets.js"
 
+dotenv.config()
 const app = express()
-const port = 5000
+const port = process.env.PORT;
 
 app.use(morgan("dev"));
 app.use(express.json());
 
+type Planet = {
+  id: number,
+  name: string,
+};
 
-// app.get("/api/planets", getAll);
+type Planets = Planet[];
 
-// app.get("/api/planets/:id", getUser);
+let planets: Planets = [
+  {
+    id: 1,
+    name: "Earth",
+  },
+  {
+    id: 2,
+    name: "Mars",
+  },
+];
 
-// app.post("/api/planets", getPost);
 
-// app.put("/api/planets/:id", getUpdate);
 
-// app.delete("/api/planets/:id", getDelete);
+app.get('/', (req, res) => {
+  console.log(req);
+
+  res.status(200).json(planets)
+
+})
 
 
 app.listen(port, () => {
