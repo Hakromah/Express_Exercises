0b795bf31fb0ac1918f9de012b9550e19f0b383f
Hassan <kkromah12@gmail.com>
2023-02-18 23:32:14 +0300

    done

--- dist/controllers/planets.js
+++ dist/controllers/planets.js
@@ -1,61 +1,72 @@
-import Joi from "Joi";
-let planets = [
-    { id: 1, name: 'Earth' },
-    { id: 2, name: 'Mars' },
-    { id: 3, name: 'Moon' },
-];
-// app.get('/api/planets', (req:Request, res) => {
-//   res.status(200).json(planets)
-// });
-// app.get("/api/planets/:id", (req:Request, res) =>{
-//    const {id} = req:Request.params;
-//    const planet = planets.find(p => p.id === Number(id))
-//    res.status(200).json(planet)
-// });
-const getAll = (req, res) => {
-    res.status(200).json(planets);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-//!GET USER
-const getUser = (req, res) => {
-    const { id } = req.params;
-    let planet = planets.find((p) => p.id === Number(id));
-    res.status(200).json(planet);
-};
-//todo  VALIDATION
-const planetSchema = Joi.object({
-    id: Joi.number().integer().required(),
-    name: Joi.string().required()
+import joi from 'joi';
+import pgPromise from 'pg-promise';
+// connection to the pgAdmin database
+const db = pgPromise()('postgres://postgres:postgres@localhost:5432/db_exercise');
+console.log(db);
+//CRETING PLANET TABLE
+const setupDb = () => __awaiter(void 0, void 0, void 0, function* () {
+    yield db.none(`
+	DROP TABLE IF EXISTS planets;
+
+	CREATE TABLE planets (
+		id SERIAL NOT NULL PRIMARY KEY,
+		name TEXT NOT NULL
+	)`);
+    yield db.none(`INSERT INTO planets (name) VALUES ('Earth')`);
+    yield db.none(`INSERT INTO planets (name) VALUES ('Mars')`);
 });
-//! POST PLANET
-const getPost = (req, res) => {
-    // const {id, name} = req:Request.body;
-    const { name } = req.body; //? create id automatically
-    // let id = planets[planets.length-1].id +1
-    const id = Math.floor(Math.random() * 100);
-    const newPlanet = { id, name };
-    const validateNewPlanet = planetSchema.validate(newPlanet); //! validation
+setupDb();
+const getAll = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const planets = yield db.many(`SELECT * FROM planets`);
+    res.status(200).json(planets);
+});
+const getOneById = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    let planet = yield db.oneOrNone(`SELECT * FROM planets WHERE id=$1`, Number(id));
+    res.status(200).json(planet);
+});
+//todo validation with (joi)
+const planetSchema = joi.object({
+    id: joi.number().integer().required(),
+    name: joi.string().required(),
+});
+// POST PLANET
+const create = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { name } = req.body;
+    //let id = planets [planets.length -1] +1 //another way to generate ID
+    let id = Math.floor(Math.random() * 100);
+    const newValue = { id, name };
+    const validateNewPlanet = planetSchema.validate(newValue);
     if (validateNewPlanet.error) {
-        return res.status(400).json({ msg: validateNewPlanet.error });
+        return res
+            .status(400)
+            .json({ msg: validateNewPlanet.error.details[0].message });
     }
     else {
-        planets = [...planets, newPlanet];
-        res.status(201).json({ msg: 'Planet was greated.' });
+        yield db.none(`INSERT INTO planets (name) VALUES ($1)`, name);
+        res.status(201).json({ msg: 'Planet was created' });
     }
-    // planets = [...planets, newPlanet];
-    // res.status(201).json({ msg: 'Tplanet was greated.' })
-};
-//!UPDATE PLANET
-const getUpdate = (req, res) => {
+});
+// UPDATE PLANET
+const updateById = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
     const { name } = req.body;
-    planets = planets.map((p) => (p.id === Number(id) ? Object.assign(Object.assign({}, p), { name }) : p));
-    console.log(planets);
-    res.status(200).json('The name is updated');
-};
-//!DELETE PLANET
-const getDelete = (req, res) => {
+    yield db.none(`UPDATE planets SET name=$2 WHERE id=$1`, [id, name]);
+    res.status(200).json({ msg: 'The name was updated' });
+});
+//DELETE PLANET
+const deleteById = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
-    planets = planets.filter((p) => p.id !== Number(id));
-    res.status(200).json({ msh: 'The planet was deleted' });
-};
-export { getAll, getUser, getPost, getUpdate, getDelete };
+    yield db.none(`DELETE FROM planets WHERE id=$1`, Number(id));
+    res.status(200).json({ msg: 'Planet was deleted' });
+});
+export { getAll, getOneById, create, updateById, deleteById };
--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,32 @@
-import express
-from 'express';
-import "express-async-errors";
-import morgan from "morgan";
-import {getAll, getUser, getPost, getUpdate, getDelete} from "./controllers/planets.js"
+import * as dotenv from 'dotenv';
+import express from 'express';
+import 'express-async-errors';
+import morgan from 'morgan';
+import {
+	create,
+	deleteById,
+	getAll,
+	getOneById,
+	updateById,
+} from './controllers/planets.js';
 
-const app = express()
-const port = 5000
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
 
+app.get('/api/planets', getAll);
 
-app.get("/api/planets", getAll);
+app.get('/api/planets/:id', getOneById);
 
-app.get("/api/planets/:id", getUser);
+app.post('/api/planets', create);
 
-app.post("/api/planets", getPost);
+app.put('/api/planets/:id', updateById);
 
-app.put("/api/planets/:id", getUpdate);
-
-app.delete("/api/planets/:id", getDelete);
-
+app.delete('/api/planets/:id', deleteById);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
-
-
-
-
-
-
+	console.log(`Example app listening on port http://localhost:${port}`);
+});
