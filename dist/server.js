e32bd8cbcdf778a4f174aeeb65c0e2d2ed3efb49
Hassan <kkromah12@gmail.com>
2023-02-20 01:51:00 +0300

    done

--- dist/server.js
+++ dist/server.js
@@ -1,16 +1,27 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import * as dotenv from 'dotenv';
 import express from 'express';
-import "express-async-errors";
-import morgan from "morgan";
-import { getAll, getUser, getPost, getUpdate, getDelete } from "./controllers/planets.js";
+import 'express-async-errors';
+import morgan from 'morgan';
+import { db } from './db.js';
+dotenv.config();
 const app = express();
-const port = 5000;
-app.use(morgan("dev"));
+const port = process.env.PORT;
+app.use(morgan('dev'));
 app.use(express.json());
-app.get("/api/planets", getAll);
-app.get("/api/planets/:id", getUser);
-app.post("/api/planets", getPost);
-app.put("/api/planets/:id", getUpdate);
-app.delete("/api/planets/:id", getDelete);
+app.get('/api/users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield db.many(`SELECT * FROM users`);
+    console.log(users);
+    res.status(200).json(users);
+}));
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`);
 });
---
+++ src/db.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import pgPromise from 'pg-promise'
+
+// connection to the pgAdmin database
+const db = pgPromise()('postgres://postgres:postgres@localhost:5432/db_exercise')
+console.log(db);
+
+
+//CRETING PLANET TABLE
+const setupDb = async () => {
+	await db.none(`
+	DROP TABLE IF EXISTS users;
+	CREATE TABLE users (
+		id SERIAL NOT NULL PRIMARY KEY,
+		username TEXT NOT NULL,
+		password TEXT NOT NULL,
+		token TEXT
+	)`)
+
+	await db.none(
+		`INSERT INTO users (username, password) VALUES ('hakromah', 'hakromah')`
+	);
+
+}
+setupDb()
+export {db};
